cae3d7cba575946cd411703b17a5f7b0cfee9435
Caio Oliveira <caio.oliveira@meta.com.br>
2021-04-28 23:58:27 -0300

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,14 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
+    'prettier/prettier': [
+      'error',
       {
-        extensions: ['.jsx', '.js'],
+        singleQuote: true,
+        semi: false,
       },
     ],
-    'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-    'no-param-reassign': 'off',
-    'no-console': 'off',
   },
-};
+  env: { jest: true },
+}
